708d80b28c02cf25d324610dee8b9eb9449ce4fb
wangjiawang <3320039466@qq.com>
2025-08-14 19:39:04 +0800

    20250814

---
+++ 2025_summer_holiday/017 score.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{
+	const int PASS=60;
+	int score;
+	
+	printf("请输入成绩：\n");
+	scanf("%d", &score);
+	if (score>=PASS)
+		printf("祝贺你，成绩及格!\n");
+	else
+		printf("很遗憾，成绩未及格。\n");
+		
+	return 0;
+
+	
+ } 
