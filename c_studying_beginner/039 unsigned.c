ed1a080384bee1261978acddf12c7777234ef360
wangjiawang <3320039466@qq.com>
2025-10-17 21:31:16 +0800

    20251017

--- c_studying_beginner/039 unsigned.c
+++ c_studying_beginner/039 unsigned.c
@@ -4,10 +4,27 @@ int main(){
 	char a = 255;
 	unsigned char b = 255;     // unsigned 的作用是变成纯二进制不考虑负数 
 	int c = 255;
+	char d = 127;
+	d = d+1;
+	char e = -128;
+	e = e-1;
+	
 	printf("a=%d,b=%d,c=%d\n", a, b, c);
-	// a: 11111111    char
-	// b: 11111111   纯二进制 255
+	// a: 11111111  char -128~127
+	// b: 11111111   纯二进制 0~255
 	// c: 00000000 00000000 0000000000 11111111
+	
+	printf("127+1=%d\n", d);
+	printf("-128-1=%d\n", e);
+	
+	unsigned char f = 255;
+	f = f+1;
+	unsigned char g = 0;
+	g = g-1;
+	
+	printf("255+1=%d\n", f);
+	printf("0-1=%d\n", g);
+	
 	
 	return 0;
 }
---
+++ c_studying_beginner/040 format.c
@@ -0,0 +1,19 @@
+/*
+整数的输入输出
+只有两种形式：int 或 long long
+%d:int
+%u:unsigned 
+%ld:long long
+%lu:usigned long long
+
+*/
+
+#include <stdio.h>
+int main(){
+	
+	char c = 012;
+	int i = 0x12;
+	printf("c=%d,i=%d\n", c, i);
+	
+	return 0;
+}
